utopia-repository/osdlyrics

commit a7d624727aad783d39bb1bb1684137ce6c91e5d1

Make ensure_unicode and ensure_utf8 no-op under Python 3.

python/utils.py
@@ -103,21 +103,27 @@ def path2uri(path):
         path = path.encode('utf8')
     return 'file://' + urllib.pathname2url(path)
 
-def ensure_unicode(value):
-    r"""
-    If value is a string, decode with utf-8. Otherwise return it directly.
-    """
-    if isinstance(value, str):
-        return value.decode('utf8')
-    return value
 
-def ensure_utf8(value):
-    r"""
-    If value is a unicode, encode with utf-8. Otherwise return it directly.
-    """
-    if isinstance(value, unicode):
-        return value.encode('utf8')
-    return value
+# TODO: remove once we fully migrate to Python 3
+if sys.version_info < (3, 0):
+    def ensure_unicode(value):
+        r"""
+        If value is a string, decode with utf-8. Otherwise return it directly.
+        """
+        if isinstance(value, str):
+            return value.decode('utf8')
+        return value
+
+    def ensure_utf8(value):
+        r"""
+        If value is a unicode, encode with utf-8. Otherwise return it directly.
+        """
+        if isinstance(value, str):
+            return value.encode('utf8')
+        return value
+else:
+    ensure_unicode = ensure_utf8 = lambda s: s
+
 
 def get_proxy_settings(config=None, conn=None):
     r"""

commit ea9ed954c039907fa7ba666e6ae9018dfd62f8ef

Fix ensure_utf8() after a7d6247
a7d6247 (in #17) broke ensure_utf8() (and the ViewLyrics plugin along
with it). Simple copy/paste mistake, simple fix.

python/utils.py
@@ -118,7 +118,7 @@ if sys.version_info < (3, 0):
         r"""
         If value is a unicode, encode with utf-8. Otherwise return it directly.
         """
-        if isinstance(value, str):
+        if isinstance(value, unicode):
             return value.encode('utf8')
         return value
 else: